bb083c5c401d275fc425b026cd4cdddb461328aa
sal-razzaq <sal.razzaq@hotmail.com>
2017-01-25 11:09:04 -0800

    Experimentation with type instantiation (Love the C# dynamic type)

---
+++ AwsApiToLambda/AwsApiToLambda/AwsApiToLambda.Tests/FirstObjRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwsApiToLambda.Tests
+{
+    public class FirstObjRequest
+    {
+        public string Val { get; set; }
+    }
+}
--- AwsApiToLambda/AwsApiToLambda/AwsApiToLambda.Tests/FunctionTest.cs
+++ AwsApiToLambda/AwsApiToLambda/AwsApiToLambda.Tests/FunctionTest.cs
@@ -8,6 +8,8 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
 
 using AwsApiToLambda;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AwsApiToLambda.Tests
 {
@@ -23,6 +25,44 @@ namespace AwsApiToLambda.Tests
             var upperCase = function.FunctionHandler("hello world", context);
 
             Assert.Equal("HELLO WORLD", upperCase);
+        }
+
+        [Fact]
+        public void TestTypeBasedRouting()
+        {
+            string typeString = "AwsApiToLambda.Tests.SecondObjRequest, AwsApiToLambda.Tests";
+            Type type = Type.GetType(typeString);
+            dynamic obj = Activator.CreateInstance(type);
+            
+            //Amazon.Lambda.Serialization.Json.JsonSerializer
+            //Newtonsoft.Json.Linq.JObject
+            Handler.Func(obj);
+        }
+
+        [Fact]
+        public void TestJsonToObject()
+        {
+            string typeString = "AwsApiToLambda.Tests.FirstObjRequest, AwsApiToLambda.Tests";
+            Type type = Type.GetType(typeString);
+            string json = "{\"val\" : 1}";
+
+
+            dynamic obj = JsonConvert.DeserializeObject(json, type);
+            Handler.Func(obj);
+        }
+
+        [Fact]
+        public void TestFullTest()
+        {
+            string jsonRequest = "{\"requestObjectType\": \"AwsApiToLambda.Tests.SecondObjRequest, AwsApiToLambda.Tests\",\"body\": \"{val : 1}\"}";
+            dynamic jsonRequestObj = JObject.Parse(jsonRequest);
+            string typeString = jsonRequestObj.requestObjectType;
+            //string typeString = "AwsApiToLambda.Tests.FirstObjRequest, AwsApiToLambda.Tests";
+            Type type = Type.GetType(typeString);
+            //string json = "{\"val\" : 1}";
+            string json = jsonRequestObj.body;
+            dynamic obj = JsonConvert.DeserializeObject(json, type);
+            object ret = Handler.Func(obj);
         }
     }
 }
